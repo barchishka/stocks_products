dd0bee96189e47eacc4a3f12925d340293a5f18d
Michail R <barchishka1982@gmail.com>
2023-10-29 00:38:15 +0300

    Initial commit

---
+++ logistic/tests.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+from rest_framework.test import APIClient
+
+
+class TestSmth(TestCase):
+    def test_sample_view_ok(self):
+        client = APIClient()
+        url = '/api/v1/test/'
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)
