f22d78da42b00d008ee542049b528b403b4a45a7
Mikhail <122753388+barchishka@users.noreply.github.com>
2023-10-29 03:55:11 +0300

    Update serializers.py

--- logistic/serializers.py
+++ logistic/serializers.py
@@ -3,14 +3,12 @@ from .models import Product, Stock, StockProduct
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
     class Meta:
         model = Product
         fields = ['id', 'title', 'description']
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
     class Meta:
         model = StockProduct
         fields = ['product', 'quantity', 'price']
@@ -23,11 +21,8 @@ class StockSerializer(serializers.ModelSerializer):
         model = Stock
         fields = ['id', 'address', 'positions']
 
-    # настройте сериализатор для склада
     def create(self, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-        # создаем склад по его параметрам
         stock = super().create(validated_data)
         for position in positions:
             StockProduct.objects.create(stock=stock, **position)
@@ -35,19 +30,12 @@ class StockSerializer(serializers.ModelSerializer):
         return stock
 
     def update(self, instance, validated_data):
-        # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-        # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
         for position in positions:
-            StockProduct.objects.update_or_create(
-                stock=stock,
-                product=position['product'],
-                defaults={
-                    'quantity': position['quantity'],
-                    'price': position['price']
-                })
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
+            StockProduct.objects.\
+                update_or_create(stock=stock,
+                                 product=position['product'],
+                                 defaults={'quantity': position['quantity'],
+                                           'price': position['price']})
         return stock
